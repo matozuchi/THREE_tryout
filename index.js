7fed1343bd764065d45ed7f63bcb3c7e2cf27ae5
M <m@Ms-MacBook-Pro.local>
2020-05-02 00:12:44 +0300

    this

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-
-app.use(express.static(path.join(__dirname)))
+const port = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, "/")))
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(process.env.PORT);
+app.listen(port);
