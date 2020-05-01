04f1d1a9ad3305b441ea08c197a42bfb604a7146
M <m@Ms-MacBook-Pro.local>
2020-05-02 00:14:53 +0300

    d

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, "/")))
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile('/index.html');
 });
 
 app.listen(port);
